de5c1edf7b05de80bb5f41b0f434ecb83bd845ad
Haswanth <Haswanth1999@gmail.com>
2024-06-17 20:06:36 -0400

    middle commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Footer.css'
+import youtube_icon from '../../assets/youtube_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
+import x_icon from '../../assets/x_icon.png'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className="footer-icons">
+        <img src={youtube_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={facebook_icon} alt="" />
+        <img src={x_icon} alt="" />
+      </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Centre</li>
+        <li>Careers</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text'>© 2025 Netflix, Inc.</p>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import './Home.css'
+import React from 'react';
+import './Home.css';
+import Navbar from '../../components/Navbar/Navbar';
+import hero_banner from '../../assets/hero_banner.jpg';
+import hero_title from '../../assets/hero_title.png';
+import play_icon from '../../assets/play_icon.png';
+import info_icon from '../../assets/info_icon.png';
+import TitleCards from '../../components/TitleCards/TitleCards';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
   return (
-    <div className='home'> 
-    
+    <div className='home'>
+      <Navbar />
+      <div className='hero'>
+        <img src={hero_banner} alt='Hero Banner' className='banner-img' />
+        <div className="hero-caption">
+          <img src={hero_title} alt='Hero Title' className='caption-img' />
+          <p> Over many missions and against impossible odds, Dom Toretto and his family have outsmarted and outdriven every foe in their path.</p>
+          <div className="hero-btns">
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+          </div>
+          <TitleCards/>
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"} category={"top_rated"}/>
+        <TitleCards title={"Only on Netflix"} category={"popular"}/>
+        <TitleCards title={"Upcoming Movies"} category={"upcoming"}/>
+        <TitleCards title={"Topics for you"} category={"now_playing"}/>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
